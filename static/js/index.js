f8eede1a956d71f4f26d0732830fd60347f341a1
swingseagull <dancyxia@gmail.com>
2015-07-20 15:51:47 -0700

    make embedded archive page and post page work

--- static/js/index.js
+++ static/js/index.js
@@ -3,8 +3,8 @@
  */
 $(document).ready(function() {
 
-    categoryDisplay();
-	archiveDisplay();
+    populateCategory();
+	populateArchive();
     generateContent();
     backToTop();
 });
@@ -51,16 +51,6 @@ function fixFooterInit() {
 function fixFooter(footerHeight, footerMarginTop) {
     var windowHeight = $(window).height();
     var contentHeight = $('body>.container').outerHeight() + $('body>.container').offset().top + footerHeight + footerMarginTop;
-    // console.log("window---"+windowHeight);
-    // console.log("$('body>.container').outerHeight()---"+$('body>.container').outerHeight() );
-    // console.log("$('body>.container').height()---"+$('body>.container').height() );
-    // console.log("$('#main').height()--------"+$('#main').height());
-    // console.log("$('body').height()--------"+$('body').height());
-    //console.log("$('#main').html()--------"+$('#main').html());
-    // console.log("$('body>.container').offset().top----"+$('body>.container').offset().top);
-    // console.log("footerHeight---"+footerHeight);
-    // console.log("footerMarginTop---"+footerMarginTop);
-    console.log(contentHeight);
     if (contentHeight < windowHeight) {
         $('footer').addClass('navbar-fixed-bottom');
     } else {
@@ -78,36 +68,32 @@ function getFooterMarginTop() {
     var margintop = $('footer').css('marginTop');
     var patt = new RegExp("[0-9]*");
     var re = patt.exec(margintop);
-    // console.log(re[0]);
     return re[0];
 }
 
-/**
- * 分类展示
- * 点击右侧的分类展示时
- * 左侧的相关裂变展开或者收起
- * @return {[type]} [description]
- */
-function categoryDisplay() {
-    /*only show All*/
-    $('.post-list-body>div[post-cate!=All]').hide();
-    /*show category when click categories list*/
-    $('.categories-list-item').click(function() {
-        var cate = $(this).attr('cate'); //get category's name
-
-        $('.post-list-body>div[post-cate!=' + cate + ']').hide(250);
-        $('.post-list-body>div[post-cate=' + cate + ']').show(400);
-    });
+//function is from here: http://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
 
-function archiveDisplay() {
-    $('.archive-list-item').click(function() {
-        var date = $(this).attr('date'); //get category's name
+function populateCategory() {
+    var cat = getParameterByName('cat');
+	 if (cat == "")
+		 cat = 'All';
+ //    $('.post-list-body>div[post-cate!=All]').hide();
+	 $('.post-list-body>div[post-cate!=' + cat + ']').hide(250);
+     $('.post-list-body>div[post-cate=' + cat + ']').show(400);	 
+}
 
-        $('.post-area>div[post-date!=' + date + ']').hide(250);
-        $('.post-area>div[post-date=' + date + ']').show(400);
-    });
-	
+function populateArchive() {
+     var date = getParameterByName('date');
+	 if (date == "")
+		 return;
+	 $('.post-area>div[post-date!=' + date + ']').hide(250);
+     $('.post-area>div[post-date=' + date + ']').show(400);	 
 }
 
 /**
@@ -130,9 +116,9 @@ function backToTop() {
     });
 
     //初始化tip
-    $(function() {
-        $('[data-toggle="tooltip"]').tooltip();
-    });
+//    $(function() {
+        //$('[data-toggle="tooltip"]').tooltip();
+//    });
 }
 
 
@@ -141,18 +127,10 @@ function backToTop() {
  */
 function generateContent() {
 
-    // console.log($('#markdown-toc').html());
     if (typeof $('#markdown-toc').html() === 'undefined') {
-        // $('#content .content-text').html('<ul><li>文本较短，暂无目录</li></ul>');
         $('#content').hide();
         $('#myArticle').removeClass('col-sm-9').addClass('col-sm-12');
     } else {
         $('#content .content-text').html('<ul>' + $('#markdown-toc').html() + '</ul>');
-        /*   //数据加载完成后，加固定边栏
-        $('#myAffix').attr({
-            'data-spy': 'affix',
-            'data-offset': '50'
-        });*/
     }
-    console.log("myAffix!!!");
 }
